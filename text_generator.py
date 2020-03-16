c7cdc08a9b8b4c28ae6964a4aa41c54d36eaa280
foksly <38046175+foksly@users.noreply.github.com>
2020-03-16 09:13:09 +0300

    Update text_generator.py

--- text_generator.py
+++ text_generator.py
@@ -2,14 +2,14 @@ from load_save import save_pkl, load_pkl
 
 
 class TextGenerator():
-    def __init__():
+    def __init__(self):
         raise NotImplementedError()
 
     def prepare_data(self, data_path=None):
         raise NotImplementedError()
 
-    def build_model(data, ngram_size=3, save_path=None):
+    def build_model(self, data, ngram_size=3, save_path=None):
         raise NotImplementedError()
 
-    def generate(length, model=None, saved_model_path=None):
+    def generate(self, length, model=None, saved_model_path=None):
         raise NotImplementedError()
